46007f8102dd7a6d437013344923125e50b0fe09
Ben <benjsl1211@gmail.com>
2020-05-02 21:55:36 +0800

    modify

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ using System.Media;
 
 namespace Snake
 {
-    //Philip - The code below is to make a structure that holds the row's variable and column's variable as global to be used
     struct Position
     {
         public int row;
@@ -35,21 +34,11 @@ namespace Snake
             int negativePoints = 0;
             int life = 3;
 
-            Console.WriteLine("Please enter your name:");
-            string name = Console.ReadLine();
-            Console.Clear();
-
             //Background music 
             SoundPlayer player = new SoundPlayer();
             player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "\\Waltz-music-loop.wav";
-            player.PlayLooping();
 
-            //add menu
-            /*Menu m = new Menu();
-            m.menu();*/
-            menu();
-            Console.Clear();
-
+         
             //max - Creates an array that has four directions
             Position[] directions = new Position[]
             {
@@ -59,18 +48,23 @@ namespace Snake
                 new Position(-1, 0), // up
             };
 
-            //max - Sets the time to 100 milliseconds
+
             double sleepTime = 100;
-            //max - Sets the direction of the snake
             int direction = right;
-            //max - Randomly generate a number
             Random randomNumbersGenerator = new Random();
-            //max - Set the height of the console
             Console.BufferHeight = Console.WindowHeight;
-            //max - Set the time for the lastFoodTime
             lastFoodTime = Environment.TickCount;
 
-            //philip - This List is to list out where would the obstacles will be appearing in the game by using X, Y Coordinator
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.Clear();
+
+
+            //add menu
+            menu();
+            Console.Clear();
+
+            //randomise obstacles
             List<Position> obstacles = new List<Position>();
 
             for (int i = 0; i < 5; i++)
@@ -84,25 +78,22 @@ namespace Snake
                 SetObstacle(obstacle);
             }
 
-            //ben - create 5 bodies of the snake 
+            //create body
             Queue<Position> snakeElements = new Queue<Position>();
             for (int i = 5; i <= 8; i++)
             {
                 snakeElements.Enqueue(new Position(0, i));
             }
 
-            //Philip - This part of code is to randomly spawn the food to any row and col, 
-            //while the food is eaten by snake or spawn at the obstacles' or snake's position, it will respawn again.
+            //randomise food
             Position food;
             do
             {
-                food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
-                    randomNumbersGenerator.Next(0, Console.WindowWidth));
+                food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth));
             }
             while (snakeElements.Contains(food) || obstacles.Contains(food));
             SetFood();
 
-            //max - Setting the color, position and the 'symbol' (which is '*') of the snakeElements.
             foreach (Position position in snakeElements)
             {
                 SetSnakeElement(position);
@@ -160,7 +151,7 @@ namespace Snake
                 if (snakeNewHead.col >= Console.WindowWidth) snakeNewHead.col = 0;
 
 
-                //ben - if snake head is collide with the body, show the word "Game over!" and show the points
+                //ben - if snake head is collide with the body, decrease 1 life
                 if (obstacles.Contains(snakeNewHead))
                 {
                     life--;
@@ -267,7 +258,7 @@ namespace Snake
                 //Add winning requirement
                 if (snakeElements.Count == 30)
                 {
-                    Win();              
+                    Win();
                     return;
                 }
 
@@ -318,23 +309,23 @@ namespace Snake
                 //int text3start = (Console.WindowWidth - text3length) / 2;
 
                 //Set Game over to middle of the window
-                Console.SetCursorPosition(text1start,y);
+                Console.SetCursorPosition(text1start, y);
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(text1);
-            
-				
+
+
                 int userPoints = (snakeElements.Count - 4) * 100 - negativePoints;
                 if (userPoints < 0) userPoints = 0;
 
                 //Set Score to middle of the window
-                Console.SetCursorPosition(text2start, y+1);
+                Console.SetCursorPosition(text2start, y + 1);
                 userPoints = Math.Max(userPoints, 0);
                 Console.WriteLine("{0}{1}", text2, userPoints);
 
                 //Add player score into plain text file.
                 StreamWriter snakeFile = new StreamWriter("Snake_Score.txt", true);
                 snakeFile.Write(name + "\n");
-                snakeFile.Write(userPoints+"\n");
+                snakeFile.Write(userPoints + "\n");
                 snakeFile.Close();
 
                 //Set instruction to middle of window
@@ -368,7 +359,7 @@ namespace Snake
                 if (userPoints < 0) userPoints = 0;
 
                 //Set Score to middle of the window
-                Console.SetCursorPosition(text2start, y+1);
+                Console.SetCursorPosition(text2start, y + 1);
                 userPoints = Math.Max(userPoints, 0);
                 Console.WriteLine("{0}{1}", text2, userPoints);
 
@@ -393,9 +384,9 @@ namespace Snake
                 Position obstacle = new Position();
                 do
                 {
-                    obstacle = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),randomNumbersGenerator.Next(0, Console.WindowWidth));
+                    obstacle = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight), randomNumbersGenerator.Next(0, Console.WindowWidth));
                 }
-                while (snakeElements.Contains(obstacle) ||obstacles.Contains(obstacle) ||(food.row != obstacle.row && food.col != obstacle.row));
+                while (snakeElements.Contains(obstacle) || obstacles.Contains(obstacle) || (food.row != obstacle.row && food.col != obstacle.row));
                 obstacles.Add(obstacle);
                 SetObstacle(obstacle);
             }
@@ -409,7 +400,6 @@ namespace Snake
                 while (snakeElements.Contains(food) || obstacles.Contains(food));
             }
 
-            //display leaderboard
             void ShowLeaderBoard()
             {
                 player.Stop();
@@ -450,6 +440,7 @@ namespace Snake
 
                     counter++;
                 }
+                file.Close();
 
                 int length = playerlist.Count();
 
@@ -489,21 +480,52 @@ namespace Snake
                     Console.WriteLine(z + "." + playerlist2[i] + "\t" + "\t" + scorelist2[i]);
                     z++;
                 }
-                Console.WriteLine("Press enter to exit");
-                file.Close();
-                Console.Read();
-                return;
+                
+                //Prompt the user to select an option after viewing leaderboard
+                string userLeadInput;
+                int userLIResult = 0;
+                bool validInput = false;
+
+                Console.Write("\n"+"Enter '1' to go back to main menu and '2' to exit the program\n");
+                userLeadInput = Console.ReadLine();
+
+                while (!validInput)
+                {
+             
+                    if (!int.TryParse(userLeadInput, out userLIResult))
+                    {
+                        Console.WriteLine("Please enter '1' or '2'");
+                    }
+                    else if (userLIResult.Equals(0))
+                    {
+                        Console.WriteLine("You cannot enter zero.");
+                    }
+                    else
+                    {
+                        validInput = true;
+                        if (userLIResult == 1)
+                        {
+                            Console.Clear();
+                            menu();
+                        }
+                        else if (userLIResult == 2)
+                        {
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+               
             }
 
             void menu()
             {
-                string userOption;
+                int userOption;
                 string condition = "correct";
                 do
                 {
-                    int ystart = (Console.WindowHeight -2 ) / 2;
+                    int ystart = (Console.WindowHeight-2) / 2;
                     string text1 = "Welcome to the Snake Menu. Please choose an option below:";
-                    string text2 = "\t\t\t(1) Play Again\t(2) View Leaderboard\t(3) Help\t(4) Quit Game";
+                    string text2 = "\t\t\t(1) Play Game\t(2) View Leaderboard\t(3) Help\t(4) Quit Game";
 
                     int text1length = text1.Length;
                     int text2length = text2.Length;
@@ -513,30 +535,31 @@ namespace Snake
 
                     //Set menu to middle of the window
                     Console.SetCursorPosition(text1start, ystart);
-                    Console.SetCursorPosition(text2start, ystart+1);
+                    Console.SetCursorPosition(text2start, ystart + 1);
                     Console.WriteLine(text1);
                     Console.WriteLine(text2);
 
-                    userOption = Console.ReadLine();
+                    userOption = Convert.ToInt32(Console.ReadLine());
 
                     switch (userOption)
                     {
-                        case "1":
+                        case 1:
                             Console.WriteLine("You have chosen option " + userOption + " -> Play the game again");
                             condition = "correct";
+                            player.PlayLooping();
                             //Program prgm = new Program();
                             break;
-                        case "2":
+                        case 2:
                             Console.WriteLine("You have chosen option " + userOption + " -> View Leaderboard");
                             condition = "correct";
                             ShowLeaderBoard();
                             break;
-                        case "3":
+                        case 3:
                             Console.WriteLine("You have chosen option " + userOption + " -> View Help Page");
                             condition = "correct";
                             //Add in help method
                             break;
-                        case "4":
+                        case 4:
                             Console.WriteLine("You have chosen option" + userOption + " -> Exit the game");
                             condition = "correct";
                             Environment.Exit(0);
